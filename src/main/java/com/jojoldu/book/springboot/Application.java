aa42940e5e3647c47e13f5d0b46ae3ec26265aec
rainrino <rainrino@naver.com>
2021-03-30 02:32:50 +0900

    dayeun 21-03-30 02:32

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,9 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+
+//@EnableJpaAuditing가 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
